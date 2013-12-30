9b33e911843c23cc1f2caf03b0c3e28b4a3e90f8
Max <max.brauer@deliveryhero.com>
2013-12-30 15:59:00 +0100

    user interface experiments

--- instagram_cl/urwid_app.py
+++ instagram_cl/urwid_app.py
@@ -4,8 +4,9 @@ import urwid
 instagram_blue = '#04a'
 palette = [('banner', '', '', '', '#ffa', instagram_blue),
            ('streak', '', '', '', 'g50', instagram_blue),
-           ('inside', '', '', '', 'g38', instagram_blue),
-           ('bg', '', '', '', 'g7', '#ccc'),]
+           ('control_bar', '', '', '', 'g38', instagram_blue),
+           ('controls', '', '', '', 'g50', instagram_blue),
+           ('background', '', '', '', 'g7', '#ccc'),]
 
 
 def exit_on_q(key):
@@ -14,17 +15,18 @@ def exit_on_q(key):
 
 
 placeholder = urwid.SolidFill()
+
 loop = urwid.MainLoop(placeholder, palette, unhandled_input=exit_on_q)
 loop.screen.set_terminal_properties(colors=256)
-loop.widget = urwid.AttrMap(placeholder, 'bg')
-loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
+loop.widget = urwid.AttrMap(placeholder, 'background')
+loop.widget.original_widget = urwid.Filler(urwid.Pile([]), 'top')
 
-divider = urwid.Divider()
-inside = urwid.AttrMap(divider, 'inside')
+controls = urwid.Text(('banner', '(r)efresh  (u)ser  (q)uit'), 'right')
+control_bar = urwid.AttrMap(controls, 'control_bar')
 text = urwid.Text(('banner', 'Instagram-CL'), align='center')
 streak = urwid.AttrMap(text, 'streak')
 pile = loop.widget.base_widget
-for item in [inside, streak, inside]:
+for item in [streak, control_bar]:
     pile.contents.append((item, pile.options()))
 
 loop.run()
