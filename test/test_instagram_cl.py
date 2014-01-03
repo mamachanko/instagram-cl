381ee6a6da4e49417c2677e3171a0514d517b94a
Max <max.brauer@deliveryhero.com>
2014-01-03 15:25:59 +0100

    remove test_instagram_cl.py

--- test/test_instagram_cl.py
+++
@@ -1,7 +0,0 @@
-import json
-import os
-
-test_dir = os.path.dirname(os.path.abspath(__file__))
-media_sample_file = os.path.join(test_dir, 'media_sample.json')
-with open(media_sample_file, 'r') as f:
-    media_sample = json.load(f)
